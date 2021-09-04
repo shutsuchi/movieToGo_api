9b5d801ab4e805679dd3559ce587408103d443a0
shutsuchi <wwww.www.wshu@gmail.com>
2021-09-04 21:17:57 +0900

    ADD GET all movies api

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -18,9 +18,11 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.logger.Println("id is", id)
-
 	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
 	// movie := models.Movie{
 	// 	ID:          id,
@@ -36,8 +38,36 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	// }
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) searchMovie(w http.ResponseWriter, r *http.Request) {
 
 }
