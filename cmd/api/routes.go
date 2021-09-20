48aff7f44ffcd69eb5491c1b1fb3f93d6a1ee477
shutsuchi <wwww.www.wshu@gmail.com>
2021-09-20 22:12:44 +0900

    ADD moviesBygenres-endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,8 +11,11 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.getOneMovie)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovie)
+	router.HandlerFunc(http.MethodGet, "/api/v1/movies/:id", app.getOneMovie)
+	router.HandlerFunc(http.MethodGet, "/api/v1/movies", app.getAllMovie)
+	router.HandlerFunc(http.MethodGet, "/api/v1/genres/:genre_id", app.getAllMoviesByGenre)
+
+	router.HandlerFunc(http.MethodGet, "/api/v1/genres", app.getAllGenres)
 
 	return app.enableCORS(router)
 }
